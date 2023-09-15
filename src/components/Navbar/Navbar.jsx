f6b909c490945885993bd095cf78d17dac8d4607
Jay Silao <jayfrancissilao12@gmail.com>
2023-09-16 07:37:12 +0800

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+//   import logo here
+import Logo from "/src/assets/NavbarLogo/logo.png";
+// import App.css
+import "/src/App.css";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -19,17 +23,53 @@ const Navbar = () => {
     window.addEventListener("scroll", handleScroll);
   }, []);
 
-  //   import logo here
-
   return (
     <header>
-      <nav>
+      <nav className="py-4 md:px-12 px-4 bg-white">
         {/* for large device */}
-        <div>
+        <div className="flex items-center justify-between ">
           {/* for logo */}
-          <div>
-            <img src="" alt="" />
+          <div className="cursor-pointer">
+            <img src={Logo} alt="" className="h-10" />
           </div>
+          {/* menu items for large devices */}
+          <div className="lg:flex items-center gap-3 hidden text-body text-lg">
+            <a
+              href="/"
+              className="block py-2 px-4 cursor-pointer text-body hover:text-gray-400 "
+            >
+              Home
+            </a>
+            <a
+              href="/"
+              className="block py-2 px-4 cursor-pointer  hover:text-gray-400 "
+            >
+              Skills
+            </a>
+            <a
+              href="/"
+              className="block py-2 px-4 cursor-pointer  hover:text-gray-400 "
+            >
+              About Me
+            </a>
+            <a
+              href="/"
+              className="block py-2 px-4 cursor-pointer  hover:text-gray-400 "
+            >
+              Portfolio
+            </a>
+            <a
+              href="/"
+              className="block py-2 px-4 cursor-pointer  hover:text-gray-400 "
+            >
+              Testimonials
+            </a>
+          </div>
+          {/* contact btn */}
+          <div className="lg:block hidden">
+            <button className="outLineBtn">Contact Me</button>
+          </div>
+          {/* menu btn for small devices */}
         </div>
       </nav>
     </header>
